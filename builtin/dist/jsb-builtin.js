3a93a2eac81b7e1d3cecbf1b3003ac03af1f601d
leda <li_cheng_zhe@163.com>
2018-07-26 11:30:06 +0800

    uncomment load() method on class FontFace (#13)

--- builtin/dist/jsb-builtin.js
+++ builtin/dist/jsb-builtin.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
 "use strict";
 
@@ -3244,11 +3244,12 @@ var FontFace = function () {
         });
     }
 
-    // Promise load() {
-    //     jsb.loadFont(this.source);
-    // }
-
     _createClass(FontFace, [{
+        key: 'load',
+        value: function load() {
+            // class FontFaceSet, add(fontFace) have done the load work
+        }
+    }, {
         key: 'status',
         get: function get() {
             return this._status;
@@ -3853,6 +3854,11 @@ var HTMLImageElement = function (_HTMLElement) {
 
             this._src = src;
             jsb.loadImage(src, function (info) {
+                if (!info) {
+                    var event = new Event('error');
+                    _this2.dispatchEvent(event);
+                    return;
+                }
                 _this2.width = _this2.naturalWidth = info.width;
                 _this2.height = _this2.naturalHeight = info.height;
                 _this2._data = info.data;
