f238c5ce45819aae19841d2681fb7360ffb2d8d4
owen <1053210246@qq.com>
2018-12-03 20:16:04 +0800

    support dragonBones bin load (#79)
    * support dragonBones bin load
    
    * support dragonBones bin load

--- dist/engine/jsb-loader.js
+++ dist/engine/jsb-loader.js
@@ -169,6 +169,7 @@ cc.loader.addDownloadHandlers({
   'fnt': downloadText,
   'binary': downloadBinary,
   'bin': downloadBinary,
+  'dbbin': downloadBinary,
   'default': downloadText
 });
 cc.loader.addLoadHandlers({
