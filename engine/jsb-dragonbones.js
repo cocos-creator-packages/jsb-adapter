ee0773e9cc16862f05d4a14820f80b36c3ce3127
owen <1053210246@qq.com>
2019-04-03 10:19:47 +0800

    Fix dynamic create dragonbones throw error (#126)
    * Fix dynamic create dragonbones throw error
    
    * sync 211 material modify

--- engine/jsb-dragonbones.js
+++ engine/jsb-dragonbones.js
@@ -442,14 +442,39 @@
         }
     };
 
+    armatureDisplayProto._activateMaterial = function () {
+        let texture = this.dragonAtlasAsset && this.dragonAtlasAsset.texture;
+        if (!texture) {
+            this.disableRender();
+            return;
+        }
+
+        // Get material
+        let material = this.sharedMaterials[0];
+        if (!material) {
+            material = cc.Material.getInstantiatedBuiltinMaterial('sprite', this);
+            material.define('_USE_MODEL', true);
+            material.define('USE_TEXTURE', true);
+        } else {
+            material = cc.Material.getInstantiatedMaterial(material, this);
+        }
+
+        material.setProperty('texture', texture);
+        this.sharedMaterials[0] = material;
+
+        this.markForUpdateRenderData(false);
+        this.markForRender(false);
+        this.markForCustomIARender(true);
+    },
+    
     armatureDisplayProto.onEnable = function () {
         renderCompProto.onEnable.call(this);
         if (this._armature) {
             this._factory.add(this._armature);
         }
-        this.node._renderFlag &= ~RenderFlow.FLAG_UPDATE_RENDER_DATA;
-        this.node._renderFlag &= ~RenderFlow.FLAG_RENDER;
-        this.node._renderFlag |= RenderFlow.FLAG_CUSTOM_IA_RENDER;
+        this.markForUpdateRenderData(false);
+        this.markForRender(false);
+        this.markForCustomIARender(true);
     };
 
     armatureDisplayProto.onDisable = function () {
