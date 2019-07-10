b7f75c2f820876b81ee94bdb1e2bf1129eed51d8
caryliu <caryliu1999@gmail.com>
2019-07-10 21:08:36 +0800

    Fix renderer error in shadow stage.

--- engine/jsb-custom-properties.js
+++ engine/jsb-custom-properties.js
@@ -1,13 +1,14 @@
 // Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
-
-class NativeCustomProperties{
-    constructor()
-    {
+let ctor2enums = cc.CustomProperties.ctor2enums
+class NativeCustomProperties {
+    constructor() {
         this._nativeObj = new renderer.CustomProperties();
     }
 
     setProperty (name, value, directly) {
-        this._nativeObj.setProperty(name, value);
+        let type = ctor2enums[value.constructor];
+        let prop = value.constructor === cc.Texture2D ? value.getImpl() : new Float32Array(value);
+        this._nativeObj.setProperty(name, type, prop);
     }
 
     define (name, value) {
