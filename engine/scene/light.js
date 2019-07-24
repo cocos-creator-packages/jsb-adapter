ae46c7e97347f632f285768f9c06a81cd5402c21
caryliu <caryliu1999@gmail.com>
2019-07-24 14:21:29 +0800

    fix shadow error of 3d-model

--- engine/scene/light.js
+++ engine/scene/light.js
@@ -23,45 +23,11 @@
  THE SOFTWARE.
  ****************************************************************************/
 
-require('./jsb-sys.js');
-require('./jsb-game.js');
-require('./jsb-videoplayer.js');
-require('./jsb-webview.js');
-require('./jsb-audio.js');
-require('./jsb-loader.js');
-require('./jsb-editbox.js');
-require('./jsb-reflection.js');
-require('./jsb-assets-manager.js');
-
-if (CC_NATIVERENDERER) {
-    require('./jsb-effect.js');
-    require('./jsb-custom-properties.js');
-    require('./scene/camera.js');
-    require('./scene/light.js');
-    require('./scene/node-proxy.js');
-    require('./scene/render-flow.js');
-    require('./scene/render-data.js');
-    // must be required after render flow
-    require('./scene/node.js');
-
-    cc.game.on(cc.game.EVENT_ENGINE_INITED, function () {
-        require('./scene/mesh-buffer.js');
-        require('./scene/quad-buffer.js');
-
-        require('./assemblers/assembler.js');
-        require('./assemblers/assembler-2d.js');
-
-        require('./assemblers/sprite/index.js');
-        require('./assemblers/label/index.js');
-        require('./assemblers/mask-assembler.js');
-        require('./assemblers/graphics-assembler.js');
-        require('./assemblers/motion-streak.js');
-        require('./assemblers/mesh-renderer.js');
-
-        require('./jsb-dragonbones.js');
-        require('./jsb-spine-skeleton.js');
-        require('./jsb-particle.js');
-        require('./jsb-tiledmap.js');
-        require('./jsb-skin-mesh.js');
-    });
-}
+var nativeLightProto = renderer.Light.prototype;
+var _setNode = nativeLightProto.setNode;
+cc.js.mixin(nativeLightProto, {
+    setNode (node) {
+        this._persistentNode = node;
+        _setNode.call(this, node);
+    }
+});
