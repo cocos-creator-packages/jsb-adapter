cd59424ff4878e4f2bddd7e25686968138178407
caryliu <caryliu1999@gmail.com>
2019-10-12 11:00:10 +0800

    modify by comment

--- engine/assemblers/assembler-3d.js
+++ engine/assemblers/assembler-3d.js
@@ -2,29 +2,37 @@
 (function(){
     if(!cc.Assembler3D) return;
 
-    const vec3 = cc.vmath.vec3;
-
-    let vec3_temps = [];
-    for (let i = 0; i < 4; i++) {
-        vec3_temps.push(vec3.create());
-    }
-
     cc.Assembler3D.updateWorldVerts = function (comp) {
             let local = this._local;
             let world = this._renderData.vDatas[0];
-            
-            vec3.set(vec3_temps[0], local[0], local[1], 0);
-            vec3.set(vec3_temps[1], local[2], local[1], 0);
-            vec3.set(vec3_temps[2], local[0], local[3], 0);
-            vec3.set(vec3_temps[3], local[2], local[3], 0);
+            let vl = local[0], vr = local[2], vb = local[1], vt = local[3];
 
+            // left bottom
             let floatsPerVert = this.floatsPerVert;
-            for (let i = 0; i < 4; i++) {
-                let vertex = vec3_temps[i];
-                let dstOffset = floatsPerVert * i;
-                world[dstOffset] = vertex.x;
-                world[dstOffset+1] = vertex.y;
-                world[dstOffset+2] = vertex.z;
-            }
+            let dstOffset = 0;
+            world[dstOffset] = vl;
+            world[dstOffset+1] = vb;
+            world[dstOffset+2] = 0;
+
+            dstOffset += floatsPerVert;
+
+            // right bottom
+            world[dstOffset] = vr;
+            world[dstOffset+1] = vb;
+            world[dstOffset+2] = 0;
+
+            dstOffset += floatsPerVert;
+
+            // left top
+            world[dstOffset] = vl;
+            world[dstOffset+1] = vt;
+            world[dstOffset+2] = 0;
+
+            dstOffset += floatsPerVert;
+
+            // right top
+            world[dstOffset] = vr;
+            world[dstOffset+1] = vt;
+            world[dstOffset+2] = 0;     
     }
 })()
