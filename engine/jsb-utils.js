34a20580010fd1c9b8d059d182743555c9733bf8
PP <chinese1236@@.com>
2019-12-12 13:56:14 +0800

    update license

--- engine/jsb-utils.js
+++ engine/jsb-utils.js
@@ -1,11 +1,28 @@
 const jsbUtils = {
-    /**
-     * JS Implementation of MurmurHash2
-     *
-     * Author: Gary Court
-     * Homepage: http://github.com/garycourt/murmurhash-js
-     * Licensed under the MIT license.
-     */
+    /****************************************************************************
+    Copyright (c) 2011 Gary Court
+   
+    http://github.com/garycourt/murmurhash-js
+   
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated engine source code (the "Software"), a limited,
+     worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+    to use Cocos Creator solely to develop games on your target platforms. You shall
+     not use Cocos Creator software for developing other software or tools that's
+     used for developing games. You are not granted to publish, distribute,
+     sublicense, and/or sell copies of Cocos Creator.
+   
+    The software or tools in this License Agreement are licensed, not sold.
+    Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+   
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    ****************************************************************************/
     murmurhash2_32_gc (str, seed) {
         var
         l = str.length,
