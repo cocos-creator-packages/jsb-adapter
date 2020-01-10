77a9551e3bf917a981a30ef3d3f5ea9596792acb
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2020-01-10 13:56:53 +0800

    impl window.devicePixelRatio #5987 (#225)
    * add window.devicePixelRatio
    
    * fix HTMLCanvasElement.getBoundingClientRect
    
    * fix
    
    * ignore set window.devicePixelRatio
    
    * use cc.Mat4 & cc.Vec3
    
    * use cc.Vec3 instead of cc.vmath.vec3
    
    * shortcut for cc.Vec3

--- builtin/jsb-adapter/HTMLCanvasElement.js
+++ builtin/jsb-adapter/HTMLCanvasElement.js
@@ -101,11 +101,11 @@ class HTMLCanvasElement extends HTMLElement {
     }
 
     get clientWidth() {
-        return this._width;
+        return this._width / window.devicePixelRatio;
     }
 
     get clientHeight() {
-        return this._height;
+        return this._height / window.devicePixelRatio;
     }
 
     get data() {
@@ -116,7 +116,7 @@ class HTMLCanvasElement extends HTMLElement {
     }
 
     getBoundingClientRect() {
-        return new DOMRect(0, 0, this._width, this._height);
+        return new DOMRect(0, 0, this._width / window.devicePixelRatio, this._height / window.devicePixelRatio);
     }
 }
 
--- builtin/jsb-adapter/window.js
+++ builtin/jsb-adapter/window.js
@@ -58,7 +58,16 @@ function inject () {
     //FIXME: The value needs to be updated when device orientation changes.
     window.orientation = orientation;
 
-    window.devicePixelRatio = 1.0;
+    // window.devicePixelRatio is readonly
+    Object.defineProperty(window, "devicePixelRatio", {
+        get: function() {
+            return jsb.device.getDevicePixelRatio ? jsb.device.getDevicePixelRatio() : 1;
+        },
+        set: function(_dpr) {/* ignore */},
+        enumerable: true,
+        configurable: true
+    });
+
     window.screen = {
         availTop: 0,
         availLeft: 0,
