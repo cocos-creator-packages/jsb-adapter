aeaf7be8fe32ff462103d8b34c1c8b11d8c70e01
PP <chinese1236@qq.com>
2020-02-25 15:17:25 +0800

    fix window innerWidth innerHeight (#249)
    * fix window innerWidth innerHeight
    
    * update

--- engine/jsb-game.js
+++ engine/jsb-game.js
@@ -37,6 +37,9 @@ jsb.onResume = function () {
 
 jsb.onResize = function (size) {
     if (size.width === 0 || size.height === 0) return;
+    // size should be the css style
+    size.width /= cc.view._devicePixelRatio;
+    size.height /= cc.view._devicePixelRatio;
     window.resize(size.width, size.height);
     cc.view.setCanvasSize(window.innerWidth, window.innerHeight);
 };
