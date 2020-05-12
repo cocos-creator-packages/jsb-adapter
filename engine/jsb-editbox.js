8ccbf533343e52ab1c4898b93f2f70621db710dd
PP <chinese1236@qq.com>
2020-05-12 15:28:09 +0800

    update editBox rect when window resized on Win32 and Mac (#286)

--- engine/jsb-editbox.js
+++ engine/jsb-editbox.js
@@ -89,6 +89,11 @@
             this._delegate = delegate;
         },
 
+        _onResize () {
+            let { x, y, width, height } = this._getRect();
+            jsb.inputBox.updateRect(x, y, width, height);
+        },
+
         beginEditing () {
             let self = this;
             let delegate = this._delegate;
@@ -135,6 +140,9 @@
             });
             this._editing = true;
             delegate.editBoxEditingDidBegan();
+            if (!cc.sys.isMobile) {
+                cc.view.on('canvas-resize', this._onResize, this);
+            }
         },
 
         endEditing () {
@@ -147,6 +155,9 @@
             }
             jsb.inputBox.hide();
             this._delegate.editBoxEditingDidEnded();
+            if (!cc.sys.isMobile) {
+                cc.view.off('canvas-resize', this._onResize, this);
+            }
         },
 
         _getRect () {
