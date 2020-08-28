1532fc269487d07aee595240a94d9162f2b97020
wuzhiming <wuzhiming.chukong@gmail.com>
2020-08-28 10:36:58 +0800

    support orientation change event and add safearea adapter for android and ios

--- engine/jsb-game.js
+++ engine/jsb-game.js
@@ -39,30 +39,9 @@ jsb.onResume = function () {
     cc.game.emit(cc.game.EVENT_SHOW);
 };
 
-function resize (size) {
-    // size should be the css style
+jsb.onResize = function (size) {
+    if (size.width === 0 || size.height === 0) return;
     size.width /= window.devicePixelRatio;
     size.height /= window.devicePixelRatio;
     window.resize(size.width, size.height);
-}
-
-jsb.onResize = function (size) {
-    if (size.width === 0 || size.height === 0) return;
-
-    // getSafeAreaEdge is asynchronous on iOS, so callback later is required
-    if (CC_JSB && cc.sys.os === cc.sys.OS_IOS) {
-        let edges = jsb.Device.getSafeAreaEdge();
-        let hasSafeArea = (edges.x > 0 || edges.y > 0 || edges.z > 0 || edges.w > 0);
-        if (hasSafeArea) {
-            setTimeout(() => {
-                if (cc.Vec4.strictEquals(edges, jsb.Device.getSafeAreaEdge())) {
-                    setTimeout(resize, 200, size);
-                } else {
-                    resize(size);
-                }
-            }, 0);
-            return;
-        }
-    }
-    resize(size);
 };
