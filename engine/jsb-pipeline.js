f09af02fda3fbd970682b382bc626d604c29f7b3
caryliu <caryliu1999@gmail.com>
2020-08-30 12:57:45 +0800

    default use js

--- engine/jsb-pipeline.js
+++ engine/jsb-pipeline.js
@@ -24,6 +24,8 @@
  ****************************************************************************/
 
 (function(){
+    if (cc.ForwardPipeline) return;
+    
     const typeToClass = {
         ForwardPipeline: nr.ForwardPipeline,
     }
