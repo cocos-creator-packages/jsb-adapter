2831b6ef61856677043142f890ecdbcfc2df4f63
caryliu <caryliu1999@gmail.com>
2020-08-30 12:44:10 +0800

    Add serialized data synchronization for pipeline.

--- engine/jsb-pipeline.js
+++ engine/jsb-pipeline.js
@@ -24,33 +24,180 @@
  ****************************************************************************/
 
 (function(){
-    if (!cc.ForwardPipeline) return;
-    const ForwardProto = cc.ForwardPipeline.prototype;
-
-    Object.assign(ForwardProto, {
-        activate () {
-            if (!this._nativeForward) {
-                this._nativeForward = new nr.ForwardPipeline();
-                this._nativeForward.init();
-            }
-
-            this._nativeForward.activate();
-
-            return true;
-        },
-
-        render (views) {
-            this._nativeForward.render(views);
+    const typeToClass = {
+        ForwardPipeline: nr.ForwardPipeline,
+    }
+    
+    class ForwardPipeline extends nr.ForwardPipeline {
+        constructor () {
+            super();
+            this._tag = 0;
+            this._flows = [];
+            this.renderTextures = [];
+            this.materials = [];
         }
+    
+        init () {
+            for (let i = 0; i < this._flows.length; i++) {
+                this._flows[i].init();
+            }
+            let info = new nr.RenderPipelineInfo(this._tag, this._flows);
+            this.initialize(info);
+        }
+    }
+    
+    class ForwardFlow extends nr.ForwardFlow {
+        constructor () {
+            super();
+            this._name = 0;
+            this._priority = 0;
+            this._tag = 0;
+            this._stages = [];
+        }
+        init () {
+            for (let i = 0; i < this._stages.length; i++) {
+                this._stages[i].init();
+            }
+            let info = new nr.RenderFlowInfo(this._name, this._priority, this._tag, this._stages);
+            this.initialize(info);
+        }
+    }
+    
+    class ShadowFlow extends nr.ShadowFlow {
+        constructor () {
+            super();
+            this._name = 0;
+            this._priority = 0;
+            this._tag = 0;
+            this._stages = [];
+        }
+        init () {
+            for (let i = 0; i < this._stages.length; i++) {
+                this._stages[i].init();
+            }
+            let info = new nr.RenderFlowInfo(this._name, this._priority, this._tag, this._stages);
+            this.initialize(info);
+        }
+    }
+    
+    class UIFlow extends nr.UIFlow {
+        constructor () {
+            super();
+            this._name = 0;
+            this._priority = 0;
+            this._tag = 0;
+            this._stages = [];
+        }
+        init () {
+            for (let i = 0; i < this._stages.length; i++) {
+                this._stages[i].init();
+            }
+            let info = new nr.RenderFlowInfo(this._name, this._priority, this._tag, this._stages);
+            this.initialize(info);
+        }
+    }
+    
+    class ForwardStage extends nr.ForwardStage {
+        constructor () {
+            super();
+            this._name = 0;
+            this._priority = 0;
+            this._tag = 0;
+            this.renderQueues = [];
+        }
+        init () {
+            const queues = [];
+            for (let i = 0; i < this.renderQueues.length; i++) {
+                queues.push(this.renderQueues[i].init());
+            }
+            let info = new nr.RenderStageInfo(this._name, this._priority, this._tag, queues);
+            this.initialize(info);
+        }
+    }
+    
+    class ShadowStage extends nr.ShadowStage {
+        constructor () {
+            super();
+            this._name = 0;
+            this._priority = 0;
+            this._tag = 0;
+        }
+        init () {
+            let info = new nr.RenderStageInfo(this._name, this._priority, this._tag, []);
+            this.initialize(info);
+        }
+    }
+    
+    class UIStage extends nr.UIStage {
+        constructor () {
+            super();
+            this._name = 0;
+            this._priority = 0;
+            this._tag = 0;
+            this.renderQueues = [];
+        }
+        init () {
+            const queues = [];
+            for (let i = 0; i < this.renderQueues.length; i++) {
+                queues.push(this.renderQueues[i].init());
+            }
+            let info = new nr.RenderStageInfo(this._name, this._priority, this._tag, queues);
+            this.initialize(info);
+        }
+    }
+    
+    class RenderQueueDesc {
+        constructor () {
+            this.isTransparent = false;
+            this.sortMode = 0;
+            this.stages = [];
+        }
+    
+        init () {
+            let desc = new nr.RenderQueueDesc(this.isTransparent, this.sortMode, this.stages);
+            return desc;
+        }
+    }
+    
+    cc.js.setClassName('ForwardPipeline', ForwardPipeline);
+    cc.js.setClassName('ForwardFlow', ForwardFlow);
+    cc.js.setClassName('ShadowFlow', ShadowFlow);
+    cc.js.setClassName('UIFlow', UIFlow);
+    cc.js.setClassName('ForwardStage', ForwardStage);
+    cc.js.setClassName('ShadowStage', ShadowStage);
+    cc.js.setClassName('UIStage', UIStage);
+    cc.js.setClassName('RenderQueueDesc', RenderQueueDesc);
+    
+    const deserializeProto = cc.deserialize._Deserializer.prototype;
+    const _deserialize = deserializeProto.deserialize;
+    
+    Object.assign(deserializeProto, {
+        deserialize (jsonObj) {
+            const kkclass = typeToClass[jsonObj.__type__];
+            if (kkclass) {
+                const data = _deserialize.call(this, jsonObj);
+                data.init();
+                return data;
+            } else {
+                return _deserialize.call(this, jsonObj);
+            }
+        },
     })
-
+    
     const RootProto = cc.Root.prototype;
-
+    
     Object.assign(RootProto, {
         createView (info) {
             const view = new nr.RenderView();
             view.initialize(info);
             return view;
+        },
+    
+        createDefaultPipeline () {
+            const pipeline = new nr.ForwardPipeline();
+            const info = new nr.RenderPipelineInfo(0, []); 
+            pipeline.initialize(info);
+            return pipeline;
         },
     })
 })();
