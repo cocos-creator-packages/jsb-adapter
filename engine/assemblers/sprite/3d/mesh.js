4d53ddfa7e460aff9449b3764bab73b4febc63bd
kenshin <420373550@qq.com>
2021-01-13 19:14:15 +0800

    fix sprite mesh mode render issue on native platform. (#362)

--- engine/assemblers/sprite/3d/mesh.js
+++ engine/assemblers/sprite/3d/mesh.js
@@ -12,7 +12,7 @@
             for (let i = 0, j = 0, l = local.length/2; i < l; i++, offset += floatsPerVert) {
                 j = i * 2;
                 world[offset] = local[j];
-                world[offset+1] = local[j++];
+                world[offset+1] = local[j+1];
                 world[offset+2] = 0;
             }
         }
