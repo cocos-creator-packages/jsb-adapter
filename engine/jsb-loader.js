ce26a782c80f9dca933f6b018e9f3b1b1da3ff1b
knox <knox.h@qq.com>
2022-11-07 15:24:19 +0800

    支持 astc 加载

--- engine/jsb-loader.js
+++ engine/jsb-loader.js
@@ -247,6 +247,7 @@ function parsePlist (url, options, onComplete) {
 
 parser.parsePVRTex = downloader.downloadDomImage;
 parser.parsePKMTex = downloader.downloadDomImage;
+parser.parseASTCTex = downloader.downloadDomImage;
 downloader.downloadScript = downloadScript;
 
 downloader.register({
@@ -266,6 +267,7 @@ downloader.register({
     '.image' : downloadAsset,
     '.pvr' : downloadAsset,
     '.pkm' : downloadAsset,
+    '.astc': downloadAsset,
 
     // Audio
     '.mp3' : downloadAsset,
@@ -328,6 +330,7 @@ parser.register({
     // compressed texture
     '.pvr': downloader.downloadDomImage,
     '.pkm': downloader.downloadDomImage,
+    '.astc': downloader.downloadDomImage,
 
     '.binary' : parseArrayBuffer,
     '.bin' : parseArrayBuffer,
