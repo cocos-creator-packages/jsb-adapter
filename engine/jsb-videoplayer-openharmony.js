98ac521f75b85167036e83a7608df3d61a2be61e
wanghui187 <348582973@qq.com>
2025-05-06 14:42:16 +0800

    Add onStop implementation to the video component
    Add onStop implementation to the video component

--- engine/jsb-videoplayer-openharmony.js
+++ engine/jsb-videoplayer-openharmony.js
@@ -235,10 +235,7 @@
     _p.stop = function () {
         let video = this._video;
         if (!video || !this._visible) return;
-        // TODO(qgh) : In the openharmony platform, there is no stop event when the video stops, instead a pause event is sent. 
-        // We can't ignore the pause event here.
-        // this._ignorePause = true;
-        video.seekTo(0);
+        this._ignorePause = true;
         video.stop();
         this._playing = false;
     };
